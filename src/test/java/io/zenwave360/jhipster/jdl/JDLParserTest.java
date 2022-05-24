beced094aac5a91b3fa05946e2611a6b53322241
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2022-05-24 09:49:28 +0200

    updates jdl-parser.js to latest upstream code.

--- src/test/java/io/zenwave360/jhipster/jdl/JDLParserTest.java
+++ src/test/java/io/zenwave360/jhipster/jdl/JDLParserTest.java
@@ -3,8 +3,15 @@ package io.zenwave360.jhipster.jdl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JDLParserTest {
 
@@ -13,6 +20,26 @@ public class JDLParserTest {
         String jdlString = loadClassPathFile("jdl-samples-main/21-points.jh");
         Map<String, Object> parsedJDL = JDLParser.parseJDL(jdlString);
         Assert.assertNotNull(parsedJDL);
+    }
+
+    @Test
+    public void testLoadAllJDLs() throws IOException {
+        Collection<String> jdlFiles = Stream.of(new File("src/test/resources/jdl-samples-main").listFiles())
+                .filter(file -> !file.isDirectory() && (file.getName().endsWith(".jh") || file.getName().endsWith(".jdl")))
+                .map(f -> "jdl-samples-main/" + f.getName())
+                .collect(Collectors.toSet());
+        Map<String, Exception> exceptions = new HashMap();
+        for (String jdlFile : jdlFiles) {
+            String jdlString = loadClassPathFile(jdlFile);
+            try {
+                System.out.println("Parsing " + jdlFile + " ...");
+                Map<String, Object> parsedJDL = JDLParser.parseJDL(jdlString);
+                Assert.assertNotNull(parsedJDL);
+            } catch (Exception e) {
+                exceptions.put(jdlFile, e);
+            }
+        }
+        Assert.assertSame(new ArrayList<>(), new ArrayList<>(exceptions.keySet()));
     }
 
     static String loadClassPathFile(String fileName) {
